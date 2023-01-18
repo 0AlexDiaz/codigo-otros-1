7fe60c46149d38805c79dac0e1b96965c50a66a2
0AlexDiaz <81484659+0AlexDiaz@users.noreply.github.com>
2023-01-18 10:52:59 -0600

    se resolvio el primer ejercicio con amor

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,20 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+async function displayUser(username) {
+  try {
+    $n.textContent = 'cargando...';
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    const data = await response.json();
+    $n.textContent = data.name;
+    $b.textContent = data.blog;
+    $l.textContent = data.location;
+  } catch (err) {
+    handleError(err);
+  }
 }
 
 function handleError(err) {
